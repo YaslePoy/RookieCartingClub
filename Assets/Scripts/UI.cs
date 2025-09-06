55a4923812d6c1471b1f45d123b822fcb14f3d31
Mihail Mitrovanov <mmm606@inbox.ru>
2025-09-06 20:33:28 +0300

    Terrain update

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -9,27 +9,25 @@ public class UI : MonoBehaviour
     public VelocityProvider VelocityProvider;
     public UIVM Uivm;
     public CartHandle Cart;
-    
-     void Update()
-     {
-         var velocity = VelocityProvider.Velocity.magnitude;
-         Uivm.Speed = velocity;
-         Uivm.UpdateSpeedKmh();
-         Uivm.LapTime = $"{TimeSpan.FromSeconds(Time.timeAsDouble - Cart.LapStart):g}";
-         if (Cart.FastestLaps.Count != 0)
-         {
-             try
-             {
-                 var sector = Time.timeAsDouble - Cart.LapStart;
-                 var delta = sector - Cart.FastestLaps[Cart.Laps.Last().Count - 1];
-                 var formated = Math.Round(delta, 2).ToString();
-                 Uivm.Delta = formated;
-             }
-             catch 
-             {
-                 
-             }
-             
-         }
-     }
+
+    void Update()
+    {
+        var velocity = VelocityProvider.Velocity.magnitude;
+        Uivm.Speed = velocity;
+        Uivm.UpdateSpeedKmh();
+        Uivm.LapTime = $"{TimeSpan.FromSeconds(Time.timeAsDouble - Cart.LapStart):g}";
+        if (Cart.FastestLaps.Count != 0)
+        {
+            try
+            {
+                var sector = Time.timeAsDouble - Cart.LapStart;
+                var delta = sector - Cart.FastestLaps[Cart.Laps.Last().Count - 1];
+                var formated = $"{Math.Round(delta, 2):N2}";
+                Uivm.Delta = formated;
+            }
+            catch
+            {
+            }
+        }
+    }
 }
