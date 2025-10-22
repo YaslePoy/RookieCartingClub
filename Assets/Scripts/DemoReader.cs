0bc65491eea525fc01243e00b60af121018d2cd6
Mikhail Mitrofanov <mmm606@inbox.ru>
2025-10-22 13:26:55 +0300

    Writing demo and remove unused terrais

---
+++ Assets/Scripts/DemoReader.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemoReader : MonoBehaviour
+{
+    public CartHandle Cart;
+
+    public List<CartStamp> cartStamps = new();
+    public bool IsRecotring;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (!IsRecotring)
+            return;
+        
+        cartStamps.Add(new CartStamp
+        {
+            position = Cart.transform.position,
+            rotation = Cart.transform.rotation
+        });
+    }
+
+    public void StartRecording()
+    {
+        cartStamps.Clear();
+        IsRecotring =  true;
+    }
+    
+    public byte[] Finish()
+    {
+        IsRecotring = false;
+        var name = new FixedString64Bytes(SceneManager.GetActiveScene().name);
+        var playerId = Cart.PlayerId.Value;
+        var data = new byte[cartStamps.Count * Marshal.SizeOf<CartStamp>()];
+        
+        Array.Copy(cartStamps.ToArray(), data, cartStamps.Count);
+        
+        var finalData = new Span<byte>(new byte[Marshal.SizeOf<FixedString64Bytes>() + Marshal.SizeOf<int>() + data.Length]);
+        MemoryMarshal.Write(finalData, ref name);
+        MemoryMarshal.Write(finalData[64..], ref playerId);
+        data.CopyTo(finalData[(64 + 4)..]);
+        
+        return finalData.ToArray();
+    }
+}
+
+public struct CartStamp
+{
+    public Vector3 position;
+    public Quaternion rotation;
+}
