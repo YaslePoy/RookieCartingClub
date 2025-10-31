84da9d69eb97939ad063e2bdba82058cfcfce1df
Mihail Mitrovanov <mmm606@inbox.ru>
2025-10-31 14:43:26 +0300

    Physics works

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,3 +1,4 @@
+using Unity.Entities;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
@@ -10,17 +11,30 @@ public class CheckPoint : MonoBehaviour
     void Start()
     {
         Collider = gameObject.GetComponent<Collider>();
-        
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Cart collider"))
             return;
-        
+
         var parent = other.transform.parent.gameObject;
 
         parent.GetComponent<CartHandle>().PushCheckPoint(this);
         //print($"Colliding {Index}");
     }
+}
+
+public class CheckPointBaker : Baker<CheckPoint>
+{
+    public override void Bake(CheckPoint authoring)
+    {
+        var entity = GetEntity(TransformUsageFlags.Dynamic);
+        AddComponent(entity, new CheckPointData { Index = authoring.Index });
+    }
+}
+
+public struct CheckPointData : IComponentData
+{
+    public int Index;
 }
