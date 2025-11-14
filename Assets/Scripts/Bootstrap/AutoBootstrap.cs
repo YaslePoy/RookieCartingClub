37f316be1775932abfb96c01a2e7edf397a9eb8d
Mikhail Mitrofanov <mmm606@inbox.ru>
2025-11-14 23:15:43 +0300

    Prepare for cleanup

---
+++ Assets/Scripts/Bootstrap/AutoBootstrap.cs
@@ -0,0 +1,11 @@
+// using Unity.NetCode;
+//
+// public class AutoBootstrap : ClientServerBootstrap
+// {
+//     public override bool Initialize(string defaultWorldName)
+//     {
+//         AutoConnectPort = 7777;
+//         this.CreateDefaultClientServerWorlds();
+//         return true;
+//     }
+// }
